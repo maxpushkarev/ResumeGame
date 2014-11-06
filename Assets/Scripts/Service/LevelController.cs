440ab224c6c7d47382d3daff210532c8c0b402b8
maxpushkarev <@pushkarevmm@gmail.com@>
2014-11-06 07:44:47 +0500

    bottles

---
+++ Assets/Scripts/Service/LevelController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace resume.Service
+{
+	public class LevelController : MonoBehaviour 
+	{
+		private const int START_LEVEL_INDEX = 0;
+		[SerializeField]
+		private GameObject[] levelPrefabs;
+		private int levelIndex;
+
+		private void OnEnable() 
+		{
+			levelIndex = START_LEVEL_INDEX;
+		}
+	
+	
+	}
+}
