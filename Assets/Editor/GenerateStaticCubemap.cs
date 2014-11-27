cd50d259f705191d1eac39cbb5337eb0364e5f0f
maxpushkarev <@pushkarevmm@gmail.com@>
2014-11-27 18:58:46 +0500

    script for reflection

---
+++ Assets/Editor/GenerateStaticCubemap.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class GenerateStaticCubemap : ScriptableWizard {
+
+	public Transform renderPosition;
+	public Cubemap cubemap;
+
+	[MenuItem("MyOwnShaders/Render cubemap")]
+	static void RenderCubeMap()
+	{
+		ScriptableWizard.DisplayWizard ("RenderCubemap", typeof(GenerateStaticCubemap), "Render!");
+	}
+
+
+	void OnWizardUpdate() 
+	{
+		helpString = "Select transform to render from and cubemap to render into";
+
+		if ((renderPosition == null) || (cubemap == null)) {
+			isValid = false;
+		} else {
+			isValid = true;
+		}
+
+	}
+
+	void OnWizardCreate() 
+	{
+		GameObject go = new GameObject("CubeCam", typeof(Camera));
+
+		go.transform.position = renderPosition.position;
+		go.transform.rotation = Quaternion.identity;
+
+		go.camera.RenderToCubemap (cubemap);
+		DestroyImmediate (go);
+	}
+
+}
