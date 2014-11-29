a7a421fecfefb394c3804d18f28fe05ab43cc70f
maxpushkarev <@pushkarevmm@gmail.com@>
2014-11-29 14:36:22 +0500

    simple vertex color

---
+++ Assets/Test/Scripts/VertexColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertexColor : MonoBehaviour {
+
+	// Sets the vertex color to be red at the y=0 and green at y=1.
+	// (Note that most builtin shaders don't display vertex colors, you can
+	// use eg. a particle shader to see vertex colors)
+	void Start () {
+
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Color[] colors  = new Color[vertices.Length];
+
+		Debug.Log (vertices.Length);
+
+		for (var i = 0; i < vertices.Length;i++)
+			colors[i] = Color.Lerp(Color.red, Color.green, vertices[i].y);
+		mesh.colors = colors;
+	}
+}
---
+++ Assets/Test/Shaders/SimpleVertex.shader
@@ -0,0 +1,30 @@
+﻿Shader "MyOwnShaders/SimpleVertex" {
+	Properties {
+		_MainTint ("Diffuse tint", Color) = (1,1,1,1)
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		#pragma surface surf Lambert vertex:vert
+
+		float4 _MainTint;
+
+		struct Input {
+			float4 vertColor;
+		};
+
+		void vert(inout appdata_full v, out Input o)
+		{
+			UNITY_INITIALIZE_OUTPUT(Input, o);
+			o.vertColor = v.color;
+		} 
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			o.Albedo = IN.vertColor.rgb * _MainTint.rgb;
+		}
+		ENDCG
+	} 
+	FallBack "Diffuse"
+}
